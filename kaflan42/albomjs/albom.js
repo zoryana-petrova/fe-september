1bf7d5d8f5d139696dc8f5ff62d2a23907b96240
kaflan <kaf_90@mail.ru>
2014-11-05 23:13:23 +0200

    my first all code

---
+++ kaflan42/albomjs/albom.js
@@ -0,0 +1,21 @@
+// <![CDATA[
+    jQuery(function() {
+        jQuery(".albom#visited").jCarouselLite({
+                btnNext: ".next#visited_next",
+                btnPrev: ".prev#visited_prev",
+                visible:4,
+                scroll:1,
+                circular: true,
+                auto: false,
+                speed: 800,
+                mouseWheel: true,
+        });
+    });
+    // ]]>
+    $( document ).ready(function() {
+
+    })
+    //     var defaultBox = $('.col-xs-6, .col-md-3').attr({
+
+    //         alt:"No listened albums"}); });
+    // $('.col-xs-6, .col-md-3').text()
