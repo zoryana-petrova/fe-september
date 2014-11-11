252f9451fac9c07fdffb9b92c845d5449b7d6fea
zhukovka <helen.zhukova@gmail.com>
2014-11-11 21:11:54 +0200

    tabs tooltips alert

--- project/js/main.js
+++ project/js/main.js
@@ -1,1 +1,21 @@
+$(document).ready(function() {
 
+	$('.tooltips').on('mouseover', function(e){
+		console.log('tooltip');
+		$(this).tooltip('show');
+	});
+
+	$('#order').on('click', function(e){
+		$('.alert-success').fadeIn();
+	});
+
+
+	$('.tab-content a[data-go="next"]').click(function (e) {
+	  e.preventDefault();
+	  // console.log($(this));
+	  var href = $(this).attr('href');
+	  // console.log('href', href);
+	  $('#navtabs a[href="'+href+'"]').tab('show');
+	});
+	
+});
